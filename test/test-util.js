f53d76a8447dbdd8ab475b84f074700999c26ccc
Kasen Lam <kasenlam@gmail.com>
2016-06-03 14:54:00 -0600

    ju-shared/issues/16: Adding new isEqual function to util library

--- test/test-util.js
+++ test/test-util.js
@@ -39,6 +39,59 @@ describe('util.js', function() {
     /**
      * Test the Util.sanitizeJson function
      */
+    it('Deep compares two objects for equality', function() {
+
+        var obj1 = {
+                        "firstName": "Ambre",
+                        "last_name": "Mart\u00edn",
+                        "birthdate": "1991-05-13",
+                        "gender": "M",
+                        "blood_type": {helloThere: 1, score : "AA" },
+                        "occupation": null,
+                        "marital_status": null,
+                        "photo": null,
+                        "phone": "123"
+        };
+
+        var obj2 = {
+                        "birthdate": "1991-05-13",
+                        "gender": "M",
+                        "firstName": "Ambre",
+                        "last_name": "Mart\u00edn",
+                        "blood_type": {score : "AA", helloThere: 1, },
+                        "occupation": null,
+                        "photo": null,
+                        "marital_status": null,
+                        "phone": "123"
+        };
+
+        var obj3 = {
+                        "last_name": "Mart\u00edn",
+                        "birthdate": "1991-05-13",
+                        "gender": "M",
+                        "blood_type": {helloThere: 1, score : "BB"},
+                        "occupation": null,
+                        "marital_status": null,
+                        "photo": null,
+                        "phone": "123",
+                        "firstName": "Ambre"
+        };
+
+
+        var isEqual = Util.isEqual(obj1, obj2),
+            isNotEqual = Util.isEqual(obj1, obj3);
+
+        // Makes sure the two objects are equal after being sanitized
+        assert.strictEqual(isEqual, true, 'Final result is incorrect');
+
+        assert.strictEqual(isNotEqual, false, 'Final result is incorrect');
+
+    });
+
+
+    /**
+     * Test the Util.sanitizeJson function
+     */
     it('sanitizeJson cleans up a JS object correctly', function() {
 
         var originalObject = {
